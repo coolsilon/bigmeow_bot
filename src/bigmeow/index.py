a13f0f246b0cb410f59d1910c54aaefed71df360
Jeffrey04 <mycyberpet@yahoo.com>
2024-06-01 15:30:29 +0800

    cache also petrol price

--- src/bigmeow/index.py
+++ src/bigmeow/index.py
@@ -1,10 +1,11 @@
 import csv
 import os
 from datetime import date, timedelta
+from enum import Enum
 from functools import reduce
 from io import BytesIO, StringIO
 from random import choice, randint, shuffle
-from typing import NamedTuple, NoReturn
+from typing import Generator, NamedTuple, NoReturn
 
 import discord
 import requests
@@ -62,12 +63,23 @@ class Latest(NamedTuple):
     change: Change
 
 
+class MeowCommand(Enum):
+    SAY = "meowsay"
+    PETROL = "meowpetrol"
+
+    def __str__(self) -> str:
+        COMMAND_PREFIX = "!"
+
+        return f"{COMMAND_PREFIX}{self.value}"
+
+
 CAT_CACHE_LIMIT = 5
 DATE_FORMAT = "%d/%m/%Y"
 
 logger = structlog.get_logger()
 client = discord.Client()
 cat_cache = Cat_Cache()
+latest_cache = Latest(Level(date.min, 0, 0, 0), Change(date.min, 0, 0, 0))
 
 
 def meowpetrol_update_latest(current: Latest, incoming: Level | Change) -> Latest:
@@ -83,51 +95,54 @@ def meowpetrol_update_latest(current: Latest, incoming: Level | Change) -> Lates
     return current._replace(**{field: incoming}) if field else current  # type: ignore
 
 
-def meowpetrol_fetch_price() -> list[str]:
-    result = []
-    result.append(
-        "Request received, fetching and parsing data from https://storage.data.gov.my/commodities/fuelprice.csv"
+def meowpetrol_fetch_price() -> Generator[str, None, None]:
+    global latest_cache
+
+    if (latest_cache.level.date + timedelta(days=6)) < date.today():
+        yield (
+            "Request received, fetching and parsing data from https://storage.data.gov.my/commodities/fuelprice.csv"
+        )
+
+        response = requests.get("https://storage.data.gov.my/commodities/fuelprice.csv")
+        latest_cache = reduce(
+            meowpetrol_update_latest,
+            [
+                Level(
+                    date.fromisoformat(row["date"]),
+                    float(row["ron95"]),
+                    float(row["ron97"]),
+                    float(row["diesel"]),
+                )
+                if row["series_type"] == "level"
+                else Change(
+                    date.fromisoformat(row["date"]),
+                    float(row["ron95"]),
+                    float(row["ron97"]),
+                    float(row["diesel"]),
+                )
+                for row in csv.DictReader(StringIO(response.text))
+            ],
+            latest_cache,
+        )
+
+    yield (
+        f"From {latest_cache.level.date.strftime(DATE_FORMAT)} to "
+        f"{(latest_cache.level.date + timedelta(days=6)).strftime(DATE_FORMAT)}"
     )
 
-    response = requests.get("https://storage.data.gov.my/commodities/fuelprice.csv")
-    latest = reduce(
-        meowpetrol_update_latest,
-        [
-            Level(
-                date.fromisoformat(row["date"]),
-                float(row["ron95"]),
-                float(row["ron97"]),
-                float(row["diesel"]),
-            )
-            if row["series_type"] == "level"
-            else Change(
-                date.fromisoformat(row["date"]),
-                float(row["ron95"]),
-                float(row["ron97"]),
-                float(row["diesel"]),
-            )
-            for row in csv.DictReader(StringIO(response.text))
-        ],
-        Latest(Level(date.min, 0, 0, 0), Change(date.min, 0, 0, 0)),
-    )
-
-    result.append(
-        f"From {latest.level.date.strftime(DATE_FORMAT)} to "
-        f"{(latest.level.date + timedelta(days=6)).strftime(DATE_FORMAT)}"
-    )
     for field in ("ron95", "ron97", "diesel"):
-        result.append(
+        yield (
             "Price of {} is RM {} per litre ({} from last week)".format(
                 {"ron95": "RON 95", "ron97": "RON 97", "diesel": "diesel"}.get(field),
-                getattr(latest.level, field),
-                "{:+0.2f}".format(getattr(latest.change, field)),
+                getattr(latest_cache.level, field),
+                "{:+0.2f}".format(getattr(latest_cache.change, field)),
             )
         )
 
-    return result
-
 
 def meow_fetch_photo() -> BytesIO:
+    global cat_cache
+
     logger.info("Fetching cat photo from cataas.com")
     response = requests.get("https://cataas.com/cat/says/meow?type=square", stream=True)
 
@@ -149,14 +164,14 @@ async def on_message(message) -> None:
     if message.author == client.user:
         return
 
-    if message.content.startswith("!meowpetrol"):
+    if message.content.startswith(str(MeowCommand.PETROL)):
         logger.info(message)
         messages = meowpetrol_fetch_price()
 
         for text in messages:
             await message.channel.send(text)
 
-    elif message.content.startswith("!meowsay"):
+    elif message.content.startswith(str(MeowCommand.SAY)):
         logger.info(message)
         await message.channel.send(
             meow_say(message.content.replace("!meowsay", "").strip())
@@ -176,39 +191,64 @@ async def on_ready() -> NoReturn:
     """
     application = ApplicationBuilder().token(os.environ["TELEGRAM_TOKEN"]).build()
 
-    application.add_handler(CommandHandler("meowpetrol", telegram_petrol))
-    application.add_handler(CommandHandler("meowsay", telegram_say))
+    application.add_handler(CommandHandler(MeowCommand.PETROL.value, telegram_petrol))
+    application.add_handler(CommandHandler(MeowCommand.SAY.value, telegram_say))
     application.add_handler(MessageHandler(filters.TEXT, telegram_meow))
 
     await application.initialize()
     await application.start()
 
-    queue = await application.updater.start_polling()
+    if application.updater:
+        queue = await application.updater.start_polling()
 
-    while True:
-        update = await queue.get()
-        logger.info("TG", update=update)
-        queue.task_done()
+        while True:
+            update = await queue.get()
+            logger.info("TG", update=update)
+            queue.task_done()
 
 
 async def telegram_petrol(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     logger.info(update)
     messages = meowpetrol_fetch_price()
 
-    for text in messages:
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+    if update.effective_chat:
+        for text in messages:
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+
 
 async def telegram_say(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     logger.info(update)
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        parse_mode=ParseMode.MARKDOWN,
-        text=meow_say(update.message.text.replace("/meowsay", "").strip()),
-    )
+
+    if update.message and update.message.text and update.effective_chat:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            parse_mode=ParseMode.MARKDOWN,
+            text=meow_say(update.message.text.replace("/meowsay", "").strip()),
+        )
 
 
 async def telegram_meow(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if "meow" in update.message.text:
+    if (
+        update.message
+        and update.effective_chat
+        and str(MeowCommand.SAY) in (update.message.text or "")
+    ):
+        logger.info(update)
+        await telegram_say(update, context)
+
+    elif (
+        update.message
+        and update.effective_chat
+        and str(MeowCommand.PETROL) in (update.message.text or "")
+    ):
+        logger.info(update)
+        await telegram_petrol(update, context)
+
+    elif (
+        update.message
+        and update.effective_chat
+        and "meow" in (update.message.text or "")
+    ):
         logger.info(update)
         await context.bot.send_photo(
             chat_id=update.effective_chat.id, photo=meow_fetch_photo()
